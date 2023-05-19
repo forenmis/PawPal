05a1bb0214516720be1e01021998c2619695413d
forenmis <forenmis@gmail.com>
2023-05-19 12:59:35 +0300

    Add contacts screen(webView)

--- app/src/main/java/com/example/pawpal/screens/home/contacts/ContactsFragment.kt
+++ app/src/main/java/com/example/pawpal/screens/home/contacts/ContactsFragment.kt
@@ -1,9 +1,11 @@
 package com.example.pawpal.screens.home.contacts
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.pawpal.databinding.FragmentContactsBinding
 
@@ -18,5 +20,15 @@ class ContactsFragment : Fragment() {
     ): View {
         binding = FragmentContactsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding.webViewContacts) {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl("https://www.mypetvet.com.ua/contact/")
+        }
     }
 }
